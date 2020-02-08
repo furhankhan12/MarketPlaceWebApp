fd959e43924048bd29aaff49343b84df674618fb
Anna Davis <amd5ef@virginia.edu>
2020-02-08 15:51:01 -0500

    updated with models and admin
    docker-compose up still not working

--- clothesmarketmicroservices/clothesmarket/models.py
+++ clothesmarketmicroservices/clothesmarket/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    price = models.IntegerField()
+    color = models.CharField(max_length=15)
+    description = models.TextField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE) # will not be using django users in the future
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE) # will not be using django users in the future
+    date = models.DateTimeField(auto_now=True)
+    deliveryMethod = models.TextField()
+    specialInstructions = models.TextField()
+
+# other things that could be modeled
+# - more specific delivery method
+# - addresses for users
+# - users
+
+# other things are could be generated in the views, but may not need models
+# - order history
+# - listing history
