6dbcfc7b3ecbf43dbac6279c1e15bc243d0c8990
Anna Davis <amd5ef@virginia.edu>
2020-02-09 11:33:20 -0500

    basically just started over with a new project

--- clothesmicroservices/clothes/admin.py
+++ clothesmicroservices/clothes/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Listing, Order, User
+from .models import Listing, Order
 
 admin.site.register(Listing)
 admin.site.register(Order)
-admin.site.register(User)
 
 # Register your models here.
 
--- clothesmicroservices/clothesmicroservices/wsgi.py
+++ clothesmicroservices/clothesmicroservices/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for clothesmarketmicroservices project.
+WSGI config for clothesmicroservices project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothesmarketmicroservices.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothesmicroservices.settings')
 
 application = get_wsgi_application()
